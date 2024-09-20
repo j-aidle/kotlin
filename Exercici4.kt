7fadf01bd899365d0ede8424f42daeba6a1ead4f
«jordi» <jordivega@iesebre.com>
2024-09-20 16:25:23 +0200

    Exercici4

---
+++ Exercici4.kt
@@ -0,0 +1,14 @@
+fun main() {
+    //declarem variables
+    var num1 = readLine()!!.toInt()
+
+    //comparació
+    if (num1 % 2 == 0) {
+        //mostrem Resultat
+        println("$num1 es parell")
+    } else {
+        //mostrem Resultat
+        println("$num1 es imparell")
+    }
+
+}
