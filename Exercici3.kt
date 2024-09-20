2389d053c4ca2b804c2422030d72e181c6c7a9cc
«jordi» <jordivega@iesebre.com>
2024-09-20 16:13:56 +0200

    Exercici3

---
+++ Exercici3.kt
@@ -0,0 +1,13 @@
+fun main() {
+    //declarem variables
+    var num1 = readLine()!!.toDouble()
+    var num2 = readLine()!!.toDouble()
+    var num3 = readLine()!!.toDouble()
+    
+    //operació
+    var mitjana :Double = (num1+num2+num3)/3
+    
+    //mostrem Resultat
+    println("Resultat: $mitjana")
+
+}
