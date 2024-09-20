23d0b9450afc829fbcc7d6b7804fc86146e2200c
«jordi» <jordivega@iesebre.com>
2024-09-20 15:56:13 +0200

    Exercici 1

---
+++ Exercici1.kt
@@ -0,0 +1,15 @@
+fun main() {
+    //declarem variables
+    var a = 3
+    var b = 9
+    
+    //operació
+    var suma = a + b
+    
+    //mostrem Resultat
+    println("Resultat: $suma")
+    
+    //mostrem Resultat
+    //println("Resultat: ${a+b}")
+    
+}
