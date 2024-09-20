2383f2f0273d554edf595a88cc58ebf4818644e4
«jordi» <jordivega@iesebre.com>
2024-09-20 16:02:25 +0200

    exercici 2

---
+++ Exercici2.kt
@@ -0,0 +1,12 @@
+fun main() {
+    //declarem variables
+    var longitud = 3
+    var amplada = 9
+    
+    //operació
+    var multiplicacio = longitud * amplada
+    
+    //mostrem Resultat
+    println("Resultat: $multiplicacio")
+    
+}
